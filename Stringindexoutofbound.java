89cc4dfbbe32331d38c07b51f9a1aeac8294342c
RAMIREDDYDEEPIKA <ramireddydeepika@gmail.com>
2024-12-10 15:09:20 +0530

    getting StringIndexOutOfBound Exception

--- Stringindexoutofbound.java
+++ Stringindexoutofbound.java
@@ -5,14 +5,16 @@ public class Stringindexoutofbound
     public static void main(String[] args)
     {
         String str="you are beautiful";
-        try{
-            for(int i=0;i<str.length();i++){
+        try
+        {
+            for(int i=0;i<str.length();i++)
+            {
                 System.out.println(str.charAt(70));
             }
         }
-        catch (StringIndexOutOfBoundsException e){
+        catch (StringIndexOutOfBoundsException e)
+        {
             System.out.println("Exception catched : "+e.getMessage());
         }
-
     }
 }
